9591e706465537019b810e173526fbcac960cd71
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-10-26 10:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function playmusic(filename)
+{
+  const audio = document.getElementById("audio");
+  audio.src = `sounds/${filename}.mp3`;
+  audio.play();
+}
+
+function stopSound()
+{
+  const audio = document.getElementById("audio");
+  audio.pause();
+  audio.currentTime = 0;
+}
